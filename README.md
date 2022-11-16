7e8755cbaaab56578535c16edfba8e343025d6c4
ike-kazu <ike.forwork@gmail.com>
2022-11-16 21:21:24 +0900

    サイトマップ作成

---
+++ README.md
@@ -0,0 +1,4 @@
+## npm install
+```
+docker-compose run --rm node sh -c 'cd yagami && npm install --save-dev next-sitemap'
+```
---
+++ yagami/sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://yagami-phi.vercel.app',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    outDir: './out',
+  };
