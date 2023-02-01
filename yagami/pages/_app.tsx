a4e16aaf54e31df455223ab2ca9df47d32e7dddd
ike-kazu <ike.forwork@gmail.com>
2023-02-01 14:05:11 +0900

    google analytics 更新

--- yagami/pages/_app.tsx
+++ yagami/pages/_app.tsx
@@ -5,6 +5,8 @@ import { CssBaseline } from '@material-ui/core'
 import { ThemeProvider } from '@material-ui/core/styles'
 import theme from '../src/theme';
 import '../styles/globals.css';
+import Script from "next/script";
+import * as gtag from "../src/lib/gtag";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   React.useEffect(() => {
@@ -22,6 +24,23 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Component {...pageProps} />
